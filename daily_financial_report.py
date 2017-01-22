6bd25a19fca2d15d738cc011c5a40c1d2f64b0a5
Zhao Zhang <zhao.zhang.glacier@gmail.com>
2017-01-22 16:43:49 -0500

    2333

--- daily_financial_report.py
+++ daily_financial_report.py
@@ -4,11 +4,14 @@ import subprocess
 import time
 
 from common_func import get_today
-
+from common_func import load_market_close_days_for_year
 if __name__ == "__main__":
     while True:
-        subprocess.call("./daily_update.py", shell=True)
-        subprocess.call("./qa_trend_continue.py 100 5", shell=True)
-        subprocess.call("./send_mail.py -s 'zzy6548 @ 126.com' '连续五日日平均交易价格趋势 %s' "
-                        "'../stock_data/report/five_days_trend/%s.txt'" % (get_today(), get_today()), shell=True)
+        today = get_today()
+        close_days = load_market_close_days_for_year('2017')
+        if today not in close_days:
+            subprocess.call("./daily_update.py", shell=True)
+            subprocess.call("./qa_trend_continue.py 100 5", shell=True)
+            subprocess.call("./send_mail.py -s 'zzy6548 @ 126.com' '连续五日日平均交易价格趋势 %s' "
+                            "'../stock_data/report/five_days_trend/%s.txt'" % (get_today(), get_today()), shell=True)
         time.sleep(86400)
