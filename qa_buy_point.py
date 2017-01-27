463d4749eeb3a867de8f3476886c2423ff34bc55
Zhao Zhang <zhao.zhang.glacier@gmail.com>
2017-01-26 22:54:05 -0500

    init ma

--- qa_buy_point.py
+++ qa_buy_point.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-import pickle
 import subprocess
 import multiprocessing as mp
 
---
+++ qa_ma.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# Moving Average System
+
+import subprocess
+import multiprocessing as mp
+import sys
+from common_func import *
+from qa_trend_continue import load_atpd_data
+from variables import *
+
