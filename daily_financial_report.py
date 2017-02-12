c1feb440f29ba9dfa7bbf67319b29e4157b05f40
Zhao Zhang <zhao.zhang.glacier@gmail.com>
2017-02-12 13:59:34 -0500

    233

--- daily_financial_report.py
+++ daily_financial_report.py
@@ -2,6 +2,9 @@
 from common_func import *
 from datetime import datetime, timedelta
 
+from qa_ma import calc_ma_for_all_stock
+from qa_trend_continue import calc_atpdr_for_all_stock, calc_atpd_for_all_stock
+
 if __name__ == "__main__":
     while True:
         today = get_today()
@@ -17,12 +20,20 @@ if __name__ == "__main__":
         print("sleeping %d" % seconds.seconds)
         time.sleep(seconds.seconds)
         if today not in close_days:
+            # update data
             subprocess.call("./daily_update.py", shell=True)
             BASIC_INFO.get_all_announcements()
+            # calculate intermediate variables
+            calc_atpd_for_all_stock()
+            calc_atpdr_for_all_stock()
+            calc_ma_for_all_stock(10)
+            calc_ma_for_all_stock(20)
+            calc_ma_for_all_stock(40)
+            # generate report
             subprocess.call("./qa_trend_continue.py 100 5 %s" % today, shell=True)
+            # send email
             subprocess.call("./send_mail.py -s '610153443@qq.com' '连续五日日平均交易价格趋势 %s' "
                             "'../stock_data/report/five_days_trend/%s.txt'" % (today, today), shell=True)
             subprocess.call("./send_mail.py -s 'zzy6548@126.com' '连续五日日平均交易价格趋势 %s' "
                             "'../stock_data/report/five_days_trend/%s.txt'" % (today, today), shell=True)
             subprocess.call("./data_news_handler.py %s" % today, shell=True)
-
--- k_plot.py
+++ k_plot.py
@@ -1,54 +1,81 @@
-#!/usr/bin/env python
-# coding=utf-8
-
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+import matplotlib
+from matplotlib import ticker
+from matplotlib.finance import *
+import pandas as pd
 import matplotlib.pyplot as plt
-import matplotlib.mlab as mlab
-
-from matplotlib.dates import DateFormatter, WeekdayLocator, DayLocator, MONDAY,YEARLY
-from matplotlib.finance import quotes_historical_yahoo, candlestick
-
 import numpy as np
 
-def main():
-    # plt.rcParams['font.sans-serif'] = ['SimHei']
-    plt.rcParams['axes.unicode_minus'] = False
-
-    ticker = '600028' # 600028 是"中国石化"的股票代码
-    ticker += '.ss'   # .ss 表示上证 .sz表示深证
-
-    date1 = (2015, 8, 1) # 起始日期，格式：(年，月，日)元组
-    date2 = (2016, 1, 1)  # 结束日期，格式：(年，月，日)元组
+from common_func import BASIC_INFO
+from variables import *
+import pickle
 
 
-    mondays = WeekdayLocator(MONDAY)            # 主要刻度
-    alldays = DayLocator()                      # 次要刻度
-    #weekFormatter = DateFormatter('%b %d')     # 如：Jan 12
-    mondayFormatter = DateFormatter('%Y-%m-%d') # 如：2-29-2015
-    dayFormatter = DateFormatter('%d')          # 如：12
+def load_stock(stock, days):
+    daily_data = pd.read_csv('../stock_data/data/%s.csv' % stock)
+    daily_data = daily_data.sort_values(by='date', ascending=True)
+    return daily_data.tail(days)
 
-    quotes = quotes_historical_yahoo(ticker, date1, date2)
 
-    if len(quotes) == 0:
-        raise SystemExit
+def load_ma_for_stock(stock, ma_params, days):
+    try:
+        with open("%s/qa/ma/%s/%s.pickle" % (stock_data_root, ma_params, stock), 'rb') as f:
+            df = pd.DataFrame.from_dict(pickle.load(f), orient='columns', dtype=None)
+    except FileNotFoundError:
+        df = None
+    df = df.sort_values(by='date', ascending=True)
+    return df.tail(days)
 
-    fig, ax = plt.subplots()
-    fig.subplots_adjust(bottom=0.2)
+def k_plot(stock, days):
+    df = load_stock(stock, days)
+    last_open= df.tail(1)['open'][0]
+    last_close = df.tail(1)['close'][0]
+    last_high = df.tail(1)['high'][0]
+    last_low = df.tail(1)['low'][0]
+    last_day_msg=' 开:%.02f 收:%.02f 高:%.02f 低:%.02f' % (last_open, last_close, last_high, last_low)
+    df_ma10= load_ma_for_stock(stock,'atpd_10', days)
+    df_ma20 = load_ma_for_stock(stock, 'atpd_20', days)
+    df_ma40 = load_ma_for_stock(stock, 'atpd_40', days)
+    # plt.ion()
+    fig = plt.figure(figsize=(12, 6), dpi=100)
 
-    ax.xaxis.set_major_locator(mondays)
-    ax.xaxis.set_minor_locator(alldays)
-    ax.xaxis.set_major_formatter(mondayFormatter)
+    N = len(df.date)
+    ind = np.arange(N)
+    date_list = df['date'].tolist()
 
-    #plot_day_summary(ax, quotes, ticksize=3)
-    candlestick(ax, quotes, width=0.6, colorup='r', colordown='g')
+    def format_date(x, pos=None):
+        thisind = np.clip(int(x + 0.5), 0, N - 1)
+        return date_list[thisind]
 
-    ax.xaxis_date()
-    ax.autoscale_view()
-    plt.setp(plt.gca().get_xticklabels(), rotation=45, horizontalalignment='right')
+    matplotlib.rcParams.update({'font.size': 14})
+    ax1 = plt.subplot2grid((4,1),(0,0), rowspan=3)
+    ax3 = plt.subplot2grid((4,1),(3,0), sharex=ax1)
+    fig.suptitle(u'%s %s 日线图 %s %s' % (stock, BASIC_INFO.name_dict[stock], df_ma10['date'].tolist()[-1],last_day_msg), fontsize=16)
 
-    ax.grid(True)
-    plt.title('600028')
-    plt.show()
-    return
-
-if __name__ == '__main__':
-    main()
+    ax1.xaxis.set_major_formatter(ticker.FuncFormatter(format_date))
+    legend_list = []
+    line_width=1.5
+    p_ma10, = ax1.plot(ind, df_ma10.ma10, '-', label=u'MA10: %s' % df_ma10['ma10'].tolist()[-1])
+    plt.setp(p_ma10, linewidth=line_width)
+    legend_list.append(p_ma10)
+    p_ma20, = ax1.plot(ind, df_ma20.ma20, '-', label=u'MA20: %s' % df_ma20['ma20'].tolist()[-1])
+    plt.setp(p_ma20, linewidth=line_width)
+    legend_list.append(p_ma20)
+    p_ma40, = ax1.plot(ind, df_ma40.ma40, '-', label=u'MA40: %s' % df_ma40['ma40'].tolist()[-1])
+    plt.setp(p_ma40, linewidth=line_width)
+    legend_list.append(p_ma40)
+    leg = ax1.legend(handles=legend_list, )
+    leg.get_frame().set_alpha(0.5)
+    candlestick2_ochl(ax1, df.open, df.close, df.high, df.low, width=0.75, colorup='r', colordown='g', alpha=1)
+    vc=volume_overlay(ax3, df.open, df.close, df.volume, colorup='r', colordown='g', width=1, alpha=1.0)
+    ax3.add_collection(vc)
+    ax1.set_axisbelow(True)
+    ax1.yaxis.grid(color='gray', linestyle='dashed')
+    ax1.xaxis.grid(color='gray', linestyle='dashed')
+    ax3.yaxis.grid(color='gray', linestyle='dashed')
+    ax3.xaxis.grid(color='gray', linestyle='dashed')
+    fig.autofmt_xdate()
+    fig.tight_layout()
+    plt.subplots_adjust(top=0.92)
+    fig.savefig('../stock_data/plots/%s.png' % stock)
