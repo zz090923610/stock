bdcbb9026b4f7c8f1802ac2efa120d0206f1b879
Zhao Zhang <zhao.zhang.glacier@gmail.com>
2017-02-17 20:49:07 -0500

    2333

--- daily_financial_report.py
+++ daily_financial_report.py
@@ -48,20 +48,20 @@ if __name__ == "__main__":
             sleep_until('19:00:00')
         if today not in close_days:
             # update data
-            #update_basic_info()
-            #update_market_open_date_list()
-            #get_update_for_all_stock()
-            #subprocess.call("./get_tick_data.py %s" % today, shell=True)
+            update_basic_info()
+            update_market_open_date_list()
+            get_update_for_all_stock()
+            subprocess.call("./get_tick_data.py %s" % today, shell=True)
             # BASIC_INFO.get_announcement_all_stock_one_day(today)
             # calculate intermediate variables
-            #calc_atpd_for_all_stock(refresh=False)
-            #calc_atpdr_for_all_stock()
-            #calc_ma_for_all_stock(3)
-            #calc_ma_for_all_stock(10)
-            #calc_ma_for_all_stock(20)
-            #calc_ma_for_all_stock(40)
-            #subprocess.call("./qa_adl.py", shell=True)
-            #subprocess.call("./qa_vhf.py", shell=True)
+            calc_atpd_for_all_stock(refresh=False)
+            calc_atpdr_for_all_stock()
+            calc_ma_for_all_stock(3)
+            calc_ma_for_all_stock(10)
+            calc_ma_for_all_stock(20)
+            calc_ma_for_all_stock(40)
+            subprocess.call("./qa_adl.py", shell=True)
+            subprocess.call("./qa_vhf.py", shell=True)
 
             # plot
             make_plots()
@@ -72,9 +72,9 @@ if __name__ == "__main__":
             print('Generating report')
             subprocess.call("./qa_trend_continue.py 100 5 %s" % today, shell=True)
             # send email
-            subprocess.call(
-                " scp '/home/zhangzhao/data/stock_data/plots/%s.html' zhangzhao@115.28.142.56:/var/www/plots/" % today,
-                shell=True)
+            #subprocess.call(
+            #    " scp '/home/zhangzhao/data/stock_data/plots/%s.html' zhangzhao@115.28.142.56:/var/www/plots/" % today,
+            #    shell=True)
             #subprocess.call("./send_mail.py -n -s '610153443@qq.com' '连续五日日平均交易价格趋势 %s' "
             #                "'../stock_data/report/five_days_trend/%s.txt'" % (today, today), shell=True)
             #subprocess.call("./send_mail.py -n -s 'zzy6548@126.com' '连续五日日平均交易价格趋势 %s' "
