597fbf84a6d746270f8cc15ac3dc527152f9c3b7
Zhao Zhang <zhao.zhang.glacier@gmail.com>
2017-07-03 07:49:59 -0400

    233

--- stock/ScriptExecutor/parser.py
+++ stock/ScriptExecutor/parser.py
@@ -7,15 +7,15 @@ from stock.common.variables import COMMON_VARS_OBJ
 
 
 def load_data(input_data):
-    print('func load_data(input_data)\n'
-          '%s %s' % (type(input_data), input_data))
+#    print('func load_data(input_data)\n'
+#          '%s %s' % (type(input_data), input_data))
     if type(input_data) == pd.core.frame.DataFrame:
         data = input_data
     elif type(input_data) == str:
         try:
             data = pd.read_csv(determine_input_path(input_data))
         except FileNotFoundError:
-            print('Load data file failed: %s' % input_data)
+#            print('Load data file failed: %s' % input_data)
             data = None
     else:
         data = None
@@ -26,7 +26,7 @@ def determine_input_path(input_path):
     if os.path.isfile(input_path):
         return input_path
     elif input_path.split('/')[0] in os.listdir(COMMON_VARS_OBJ.QA_DIR):
-        if os.path.isfile(COMMON_VARS_OBJ.QA_DIR + '/' + input_path):
+        if os.path.exists(COMMON_VARS_OBJ.QA_DIR + '/' + input_path):
             return COMMON_VARS_OBJ.QA_DIR + '/' + input_path
     else:
         return input_path
@@ -38,7 +38,7 @@ def determine_output_path(output_path):
     elif output_path.split('/')[0] in os.listdir(COMMON_VARS_OBJ.QA_DIR):
         return COMMON_VARS_OBJ.QA_DIR + '/' + output_path
     else:
-        return output_path
+        return COMMON_VARS_OBJ.QA_DIR + '/' + output_path
 
 
 def load_script(script_path):
@@ -77,13 +77,15 @@ def parse_script_head(script_path):
         if line[0] == 'PLEV':
             parallel_level = line[1]
         elif line[0] == 'FIN':
-            input_dir_file = line[1]
+            input_dir_file =determine_input_path( line[1])
         elif line[0] == 'PLFDOUT':
-            output_dir_file = line[1]
+            output_dir_file = determine_output_path(line[1])
         elif line[0] == 'SGFLOUT':
-            output_dir_file = line[1]
+            output_dir_file = determine_output_path(line[1])
         elif line[0] == 'OUTCOLS':
             output_cols = re.split(r',', line[1])
+    print(23333, input_dir_file)
+
     print("Parallel level: %s\nInput path: %s\n Output path: %s\n Output cols: %r" %
           (parallel_level, input_dir_file, output_dir_file, output_cols))
     return script, parallel_level, input_dir_file, output_dir_file, output_cols
@@ -94,11 +96,13 @@ def engine(script_path):
     if parallel_level == 'FOLDER':
         dir_list = os.listdir(input_dir_file)
         input_dir = input_dir_file.rstrip('/')
+        print(input_dir)
         output_dir = output_dir_file.rstrip('/')
-        os.makedirs(output_dir)
+        if not os.path.exists(output_dir):
+            os.makedirs(output_dir)
         pool = mp.Pool()
         for i in dir_list:
-            pool.apply_async(execute_script, args=(i, input_dir + '/' + i, output_dir + '/' + i, output_cols))
+            pool.apply_async(execute_script, args=(input_dir + '/' + i, script, output_dir + '/' + i, output_cols))
         pool.close()
         pool.join()
     elif parallel_level == 'SINGLE':
@@ -143,10 +147,12 @@ class ScriptVariable:
         elif self.v_type == 'col':
             return [self.v_name]
         elif self.v_type == 'series':
-            if pass_in != '':
+            if pass_in != self.v_name:
                 return self.gen_sub_series_list(pass_in)
             else:
                 return self.gen_sub_series_list(self.v_name)
+    def print_self(self):
+        print(self.v_name,self.v_type,self.v_val,self.series)
 
 
 class VariableHdl:
@@ -163,8 +169,8 @@ class VariableHdl:
         self.vdict[v_name] = ScriptVariable(v_type, v_name, v_val=v_val, series=series)
 
     def respond_var(self, v_name):
-        if v_name in self.vdict.keys():
-            return self.vdict[v_name].respond(pass_in=v_name)
+        if v_name.split('_')[0] in self.vdict.keys():
+            return self.vdict[v_name.split('_')[0]].respond(pass_in=v_name)
         elif is_num(v_name):
             return [float(v_name)]
         else:
@@ -184,41 +190,61 @@ def execute_script(input_data, script, output_path, output_cols):
             opts_cols = []
             for op_source in line[2:]:
                 opts_cols += var_hdl.respond_var(op_source)
-            result_col_name = opts_cols[0]
-            for col in opts_cols[1:]:
-                data[result_col_name] += data[col]
+            result_col_name = line[1]
+            data[result_col_name] = 0
+            for col in opts_cols:
+                if is_num(col):
+                    data[result_col_name] += col
+                else:
+                    data[result_col_name] += data[col]
             var_hdl.add_var(result_col_name, 'col')
         elif line[0] == 'SUBC':
             opts_cols = []
             for op_source in line[2:]:
                 opts_cols += var_hdl.respond_var(op_source)
-            result_col_name = opts_cols[0]
+            result_col_name = line[1]
+            data[result_col_name] =data[opts_cols[0]]
             for col in opts_cols[1:]:
-                data[result_col_name] -= data[col]
+                if is_num(col):
+                    data[result_col_name] -= col
+                else:
+                    data[result_col_name] -= data[col]
             var_hdl.add_var(result_col_name, 'col')
         elif line[0] == 'MULC':
             opts_cols = []
             for op_source in line[2:]:
                 opts_cols += var_hdl.respond_var(op_source)
-            result_col_name = opts_cols[0]
+            result_col_name = line[1]
+            data[result_col_name] = data[opts_cols[0]]
             for col in opts_cols[1:]:
-                data[result_col_name] *= data[col]
+                if is_num(col):
+                    data[result_col_name] *= col
+                else:
+                    data[result_col_name] *= data[col]
             var_hdl.add_var(result_col_name, 'col')
         elif line[0] == 'DIVC':
             opts_cols = []
             for op_source in line[2:]:
                 opts_cols += var_hdl.respond_var(op_source)
-            result_col_name = opts_cols[0]
+            result_col_name = line[1]
+            data[result_col_name] = data[opts_cols[0]]
             for col in opts_cols[1:]:
-                data[result_col_name] /= data[col]
+                if is_num(col):
+                    data[result_col_name] /= col
+                else:
+                    data[result_col_name] /= data[col]
             var_hdl.add_var(result_col_name, 'col')
         elif line[0] == 'MODC':
             opts_cols = []
             for op_source in line[2:]:
                 opts_cols += var_hdl.respond_var(op_source)
-            result_col_name = opts_cols[0]
+            result_col_name = line[1]
+            data[result_col_name] = data[opts_cols[0]]
             for col in opts_cols[1:]:
-                data[result_col_name] %= data[col]
+                if is_num(col):
+                    data[result_col_name] %= col
+                else:
+                    data[result_col_name] %= data[col]
             var_hdl.add_var(result_col_name, 'col')
         elif line[0] == 'SQRTC':
             result_col_name = line[1]
@@ -252,5 +278,6 @@ def execute_script(input_data, script, output_path, output_cols):
             val = float(line[2])
             result_col_name = line[1]
             var_hdl.add_var(result_col_name, 'imm', v_val=val)
-    data[output_cols].to_csv(determine_output_path(output_path), index=False)
+
+    data[output_cols].to_csv(output_path, index=False)
     # return data,shift_series
