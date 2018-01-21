eef40767d66673729dede9cdcb954cb1cdcdc379
Zhao Zhang <zhao.zhang.glacier@gmail.com>
2018-01-21 09:41:48 -0500

    233

--- misc/install_chromedriver.sh
+++ misc/install_chromedriver.sh
@@ -4,7 +4,7 @@ sudo apt-get install -y unzip openjdk-8-jre-headless xvfb libxi6 libgconf-2-4
 
 CHROME_DRIVER_VERSION=`curl -sS http://cdn.npm.taobao.org/dist/chromedriver/LATEST_RELEASE`
 # Install ChromeDriver.
-wget -N https://chromedriver.storage.googleapis.com/$LATEST_RELEASE/chromedriver_linux64.zip -P ~/
+wget -N https://chromedriver.storage.googleapis.com/$CHROME_DRIVER_VERSION/chromedriver_linux64.zip -P ~/
 unzip ~/chromedriver_linux64.zip -d ~/
 rm ~/chromedriver_linux64.zip
 sudo mv -f ~/chromedriver /usr/local/bin/chromedriver
