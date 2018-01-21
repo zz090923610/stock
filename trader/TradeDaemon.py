6bef6b3ec204d4af6d2c0c2e7f84402dedc6ba64
Zhao Zhang <zhao.zhang.glacier@gmail.com>
2018-01-21 13:19:45 -0500

    233

--- trader/TradeDaemon.py
+++ trader/TradeDaemon.py
@@ -8,19 +8,6 @@ from trader.account_info import AUTH
 from trader.api_driver import TradeAPI
 
 
-class StoppableThread(threading.Thread):
-    """Thread class with a stop() method. The thread itself has to check
-    regularly for the stopped() condition."""
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self._stop_event = threading.Event()
-
-    def stop(self):
-        self._stop_event.set()
-
-    def stopped(self):
-        return self._stop_event.is_set()
 
 
 class TradeDaemon(DaemonClass):
@@ -28,25 +15,25 @@ class TradeDaemon(DaemonClass):
         super().__init__(topic_sub=topic_sub, topic_pub=topic_pub, auth=auth)
         self.trade_api = TradeAPI(headless=True, auth=AUTH)
         self.captcha_db = {}
-        self.heart_thread = StoppableThread(target=self.heart_beat, daemon=True)
+        self.heart_thread = None
 
     def heart_beat(self):
+        self.cancel_daemon = False
         self.trade_api.respond('TradeDaemon/heartbeat started')
+        cnt = 0
         while not self.cancel_daemon:
-            time.sleep(120)
-            self.trade_api.send_heartbeat()
+            time.sleep(.5)
+            cnt += 1
+            if cnt == 240:
+                self.trade_api.send_heartbeat()
+                cnt = 0
             if self.trade_api.status != 'active':
                 return
 
     def mqtt_on_message(self, mqttc, obj, msg):
         payload = msg.payload.decode('utf8')
         if payload == 'prelogin':
-            try:
-                self.heart_thread.stop()
-                self.heart_thread.join()
-                self.heart_thread = StoppableThread(target=self.heart_beat, daemon=True)
-            except Exception as e:
-                pass
+            self.cancel_daemon = True
             self.trade_api.pre_login()
         elif payload == 'cash':
             self.trade_api.get_available_cash()
@@ -55,6 +42,7 @@ class TradeDaemon(DaemonClass):
         elif payload.split("_")[0] == "login":
             self.trade_api.login_with_verify_code(payload.split("_")[1])
             if self.trade_api.status == 'active':
+                self.heart_thread = threading.Thread(target=self.heart_beat, daemon=True)
                 self.heart_thread.start()
         elif payload.split("_")[0] == "buy":
             (b, symbol, price, quant) = payload.split("_")
