36b18318d2a4d58a81331556c179e7931ef9d3a6
Zhao Zhang <zhao.zhang.glacier@gmail.com>
2018-01-21 13:13:53 -0500

    233

--- trader/TradeDaemon.py
+++ trader/TradeDaemon.py
@@ -8,12 +8,27 @@ from trader.account_info import AUTH
 from trader.api_driver import TradeAPI
 
 
+class StoppableThread(threading.Thread):
+    """Thread class with a stop() method. The thread itself has to check
+    regularly for the stopped() condition."""
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._stop_event = threading.Event()
+
+    def stop(self):
+        self._stop_event.set()
+
+    def stopped(self):
+        return self._stop_event.is_set()
+
+
 class TradeDaemon(DaemonClass):
-    def __init__(self, topic_sub='trade_req', topic_pub='trade_res/str',auth=None):
-        super().__init__(topic_sub=topic_sub, topic_pub=topic_pub,auth=auth)
-        self.trade_api = TradeAPI(headless=True,auth=AUTH)
+    def __init__(self, topic_sub='trade_req', topic_pub='trade_res/str', auth=None):
+        super().__init__(topic_sub=topic_sub, topic_pub=topic_pub, auth=auth)
+        self.trade_api = TradeAPI(headless=True, auth=AUTH)
         self.captcha_db = {}
-        self.heart_thread = threading.Thread(target=self.heart_beat, daemon=True)
+        self.heart_thread = StoppableThread(target=self.heart_beat, daemon=True)
 
     def heart_beat(self):
         self.trade_api.respond('TradeDaemon/heartbeat started')
@@ -26,6 +41,12 @@ class TradeDaemon(DaemonClass):
     def mqtt_on_message(self, mqttc, obj, msg):
         payload = msg.payload.decode('utf8')
         if payload == 'prelogin':
+            try:
+                self.heart_thread.stop()
+                self.heart_thread.join()
+                self.heart_thread = StoppableThread(target=self.heart_beat, daemon=True)
+            except Exception as e:
+                pass
             self.trade_api.pre_login()
         elif payload == 'cash':
             self.trade_api.get_available_cash()
@@ -41,7 +62,6 @@ class TradeDaemon(DaemonClass):
         elif payload.split("_")[0] == "sell":
             (s, symbol, price, quant) = payload.split("_")
             threading.Thread(target=self.trade_api.sell, args=(symbol, price, quant)).start()
-
 
 
 if __name__ == '__main__':
