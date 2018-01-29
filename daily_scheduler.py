81b92d267391d9a3a46b4c5864553250cfca8288
Zhao Zhang <zhao.zhang.glacier@gmail.com>
2018-01-29 09:09:08 -0500

    2333

---
+++ daily_scheduler.py
@@ -0,0 +1,16 @@
+import os
+
+import schedule
+import time
+# DEPENDENCY( schedule)
+
+def daily_analysis():
+    os.system('./daily.sh')
+
+
+if __name__ == '__main__':
+    schedule.every().day.at("08:58").do(daily_analysis)
+    while True:
+
+        schedule.run_pending()
+        time.sleep(1)
