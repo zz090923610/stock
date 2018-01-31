b448d85499e4fe4bfe2ab7725889ba61c1d7d435
zhao zhang <zhao.zhang.glacier@gmail.com>
2018-01-31 08:26:39 -0500

    233

--- daily.sh
+++ daily.sh
@@ -5,6 +5,6 @@ python3 -m tools.ctrl_hdl ctrls/everyday_analysis_v2.ctrl
 python3 -m tools.ctrl_hdl ctrls/anomaly.ctrl
 cp ../stockdata/naive_score/*.csv ~/
 cp ../stockdata/slice/*.csv ~/
-eval `python3 -m -z misc.zip_report_send TODAY`
-eval `python3 -m -s misc.zip_report_send TODAY`
+eval `python3 -m misc.zip_report_send -z TODAY`
+eval `python3 -m misc.zip_report_send -s TODAY`
 rm ~/*.csv # todo: warning don't save any csv on ~/
